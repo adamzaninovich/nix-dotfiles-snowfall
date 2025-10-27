6779e574bbd67cc4b9d9a98fba34d02e72d112b6
Adam Zaninovich <adam.zaninovich@gmail.com>
2025-10-27 10:48:29 -0700

    remove extra substituters

--- flake.nix
+++ flake.nix
@@ -3,17 +3,6 @@
 
   nixConfig = {
     experimental-features = [ "nix-command" "flakes" ];
-    # substituters = [
-    #   "https://cache.nixos.org"
-    # ];
-    #
-    # extraSubstituters = [
-    #   "https://nix-community.cachix.org"
-    # ];
-    #
-    # extra-trusted-public-keys = [
-    #   "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    # ];
   };
 
   inputs = {
