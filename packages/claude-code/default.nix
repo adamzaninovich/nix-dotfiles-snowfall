ad1cd2e24840cbb231a21c29fab12560d293af71
Adam Zaninovich <adam.zaninovich@gmail.com>
2026-02-06 11:36:27 -0800

    update claude

--- packages/claude-code/default.nix
+++ packages/claude-code/default.nix
@@ -2,10 +2,10 @@
 
 pkgs.claude-code.overrideAttrs (oldAttrs: {
   pname = "claude-code";
-  version = "2.1.32";
+  version = "2.1.34";
 
   src = pkgs.fetchurl {
-    url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-2.1.32.tgz";
-    sha256 = "1v811dwpqj3yfyghdw9zwiw7l8xdgq3zb8bnx3w7jnkihnqqzh58";
+    url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-2.1.34.tgz";
+    sha256 = "0l47b1sfz7jsp1hqw28ri972cr82f0rw31hrqgrmsrjy72qj96pn";
   };
 })
